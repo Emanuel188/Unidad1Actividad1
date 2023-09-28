e2d56fb6510416bf6d693ceff20db4d82c559f24
ManuRamos <ManuRamos@MANU>
2023-09-28 14:31:47 -0600

    Agregar archivos de proyecto.

---
+++ Unidad1Actividad1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unidad1Actividad1.Models;
+
+namespace Unidad1Actividad1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            MiPerfilViewModel perfil = new();
+            perfil.Nombre = "Sergio Emanuel Ramos Rodriguez";
+            perfil.Materia = "Programacion Web";
+            perfil.Semestres = 7;
+            perfil.Periodo = "Agosto-Diciembre 2023";
+            return View(perfil);
+        }
+        public IActionResult MiPerfil()
+        {
+           
+            return View();
+        }
+       
+    }
+}
+
+    
---
+++ Unidad1Actividad1/Models/MiPerfilViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Unidad1Actividad1.Models
+{
+    public class MiPerfilViewModel
+    {
+        public string? Nombre { get; set; }
+        public string? Materia { get; set; }
+        public string? Periodo { get; set; }
+        public int? Semestres { get; set; }
+    }
+}
